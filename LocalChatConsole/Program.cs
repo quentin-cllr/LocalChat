0d2eab0949133635ece0030945baca4c6098476c
quentin-cllr <qcoullery@gmail.com>
2026-01-20 21:47:40 +0100

    update serveur-client

--- LocalChatConsole/Program.cs
+++ LocalChatConsole/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
-Console.WriteLine("Hello!");
+class Program
+{
+    const int PORT = 5000;
+
+    static void Main()
+    {
+        TcpListener server = null;
+
+        try
+        {
+            server = new TcpListener(IPAddress.Any, PORT);
+            server.Start();
+            Console.WriteLine("Mode serveur");
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Client connecté");
+            }
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("Mode client");
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.1", PORT);
+            Console.WriteLine("Connecté au serveur");
+        }
+
+        Console.ReadLine();
+    }
+}
